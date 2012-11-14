cc004adc74de7d5fc792618eb93d3ffbf6e2c5c5
kocsenc@rubiksLife <kocsenc@gmail.com>
2012-11-14 14:52:46 -0500

    wssa

--- ScrabbleSolver/scrabblesolver.rb
+++ ScrabbleSolver/scrabblesolver.rb
@@ -26,7 +26,7 @@ def main()
 
 		while boolean
 			puts
-			puts "   -1 to exit \n    1 To make a move!\n    2 To print out dictionary"
+			puts "   -1 to exit \n    1 To make a move!\n    2 To find in dictionary"
 			print ">> "
 			command = gets.chomp!
 			
@@ -37,16 +37,22 @@ def main()
 
 				puts "Please insert your letters separated by a SPACE."
 				hand = gets.chomp!.upcase
-				puts "This is your input> " + hand
+				
 				puts "This is your input sorted: " + hand.split(' ').sort.join.upcase
 				hand = hand.split(' ').sort.join
-				print getPossibleWords(hand, anagramHash).inspect
+				
+
+				resultArray =  everyPermutation(hand, anagramHash, pointHash)
+				puts resultArray.inspect
 
 				
 
 
 			when "2"
-				print dictionary
+				puts "Please insert word to search:"
+				input = gets.chomp!.split(//).sort.join.upcase
+				searchWord(input, anagramHash)
+
 
 			end
 		end
@@ -57,28 +63,136 @@ def main()
 		
 end
 
-def getPossibleWords(input, anagramHash)
-	puts sysout("Looking for possible words...")
-	anagramHash.each {|sortedWord,actualWords| 
-		#puts sysout"Searching through " + sortedWord
-		if sortedWord.eql? input
-			puts
-			print sysout"FOUND MATCH! -> "
-			return actualWords
-		
-		end
+def everyPermutation(word, anagramHash, pointHash)
+	puts sysout("Word is " + word)
+	#puts sysout("Word split(//) is " + word.split(//).inspect)
 
-		}
-	puts sysout"SORRY NO MATCHES"
-	return []	
+	mainWord = word.clone()
+	puts sysout("Word clone is " + mainWord)
+
+	wordSize = mainWord.split(//).size
+	puts sysout("wordSize = " + wordSize.to_s())
+
+	counter = 0
+	permCounter = 0
+	maxPointsAttained = 0
+	results = []
+	beenThereDoneThat = []
+
+	#allTheWords = word.chars.to_a.permutation.map &:join
 	
+	puts sysout("Searching for ALL results...")
+	while permCounter < wordSize
+		
+		while counter < wordSize 
+			canCheck1 = true
+			canCheck2 = true
+			
+			wiq = word.split(//)
+			#split1 = wiq[counter, wordSize].sort.join
+
+			split1 = ((wiq.slice(counter,permCounter)  + wiq.slice(counter+1 , wordSize)))    
+			split1 = split1.sort.join
+			if split1.split(//).size < 2 or beenThereDoneThat.include?(split1) 
+				beenThereDoneThat.push()
+				canCheck1 = false
+			end
+
+
+
+			#split2 = x.split(//)[0 ,counter].sort.join
+
+
+			split2 = ((wiq.slice(0, counter) + wiq.slice(counter+1+permCounter, wordSize)))
+			split2 = split2.sort.join
+			if split2.split(//).size < 2 or beenThereDoneThat.include?(split2)
+				canCheck2 = false
+			end
+			beenThereDoneThat.push(split1)
+			beenThereDoneThat.push(split2)
+
+			if split1.eql? split2
+				puts sysout("NOTICE: SPLIT 1 = SPLIT 2")
+			end
+
+
+			puts sysout(canCheck2.inspect)
+			if canCheck1
+				queryResult1 = getPossibleWords(split1, anagramHash)
+				if queryResult1 != []
+					queryResult1.each { |q| 
+						if !results.include?(q) and getPoints(q, pointHash) > maxPointsAttained
+							#puts sysout("Pushing " +q)
+							results.push(q)
+						else
+							#puts sysout("Found duplicate, not pushing: " + q)
+						end
+						}
+					#puts sysout("Since no matches, no push")
+					
+				end
+			end
+
+			puts sysout(canCheck2.inspect)
+			if canCheck2
+				queryResult2 = getPossibleWords(split2, anagramHash)
+				if queryResult2 != [] 
+					queryResult2.each { |w|
+						if !results.include?(w) and getPoints(w, pointHash) > maxPointsAttained
+							#puts sysout("Pushing " +w)
+							results.push(w)
+						else
+							#puts sysout("Found duplicate, not pushing: " + w)
+						end
+					}
+					
+				end
+			end
+			
+			counter += 1
+			
+		end
+		permCounter += 1
+		counter = 0
+
+		
+	end
+	return results 
+
 end
 
 
+def getPossibleWords(input, anagramHash)
+	#puts sysout("Looking for words that match...")
+
+	if anagramHash.has_key?(input)
+		#puts sysout("FOUND MATCH! -> " + anagramHash.fetch(input).inspect)
+		return anagramHash.fetch(input)
+	else 
+		return []
+
+	end
 
 
+	
+end
 
+def getPoints(word, pointHash)
+	points = 0
+	word.split(//).each {|x|
+		points += pointHash.fetch(x)
+	}
+	return points
+end
 
+def searchWord(input, anagramHash)
+	query = getPossibleWords(input, anagramHash)
+	if query != []
+		puts "YES that word exists!"
+	else
+		puts "NO, sorry word not found."
+	end
+end
 
 
 
