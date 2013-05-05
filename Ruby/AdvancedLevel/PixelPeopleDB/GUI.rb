285de0167983d531c00d8a4d698656b9f1918dda
Kocsen Chung <kocsenc@gmail.com>
2013-05-05 01:54:51 -0400

    made GUI module a class

--- Ruby/AdvancedLevel/PixelPeopleDB/GUI.rb
+++ Ruby/AdvancedLevel/PixelPeopleDB/GUI.rb
@@ -1,4 +1,10 @@
 # Kocsen Chung
 # Main GUI Class that uses TK to represent the program with a front end
 
+require 'tk'
 
+class GUI
+	def initialize()
+		root = TkRoot.nes {title "Hello"}
+	end
+main()
