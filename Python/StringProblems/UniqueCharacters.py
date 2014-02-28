15f1502110f93bef301704b5b55d2b74b7d9a012
Kocsen <kxc4519@rit.edu>
2014-02-28 18:42:42 -0500

    Tabs -> Spaces because PEP 8

--- Python/StringProblems/UniqueCharacters.py
+++ Python/StringProblems/UniqueCharacters.py
@@ -7,57 +7,58 @@
 import sys
 import unittest
 
-def is_unique(str):
-	# Cannot be longer thatn 256 chars without repeating
-	if len(str) >= 256:
-		return False
-	
 
-	present_ary = []
-	for char in str:
-		if char in present_ary:
-			return False
-		else:
-			present_ary.append(char)
-	return True
+def is_unique(str):
+    # Cannot be longer than 256 chars without repeating
+    if len(str) >= 256:
+        return False
+
+    present_ary = []
+    for char in str:
+        if char in present_ary:
+            return False
+        else:
+            present_ary.append(char)
+    return True
 
 
 class UniqueCharactersTest(unittest.TestCase):
-	"""
-	Unit tests for is_unique method
-	"""
+    """
+    Unit tests for is_unique method
+    """
 
-	def test_empty_string(self):
-		""" Tests empty string"""
-		self.assertTrue(is_unique(""))
+    def test_empty_string(self):
+        """ Tests empty string"""
+        self.assertTrue(is_unique(""))
 
-	def test_passing_words(self):
-		""" Normal passing words"""
-		self.assertTrue(is_unique(" "))
-		self.assertTrue(is_unique("abc"))
-		self.assertTrue(is_unique("yole"))
-		self.assertTrue(is_unique("aeiou"))
-		self.assertTrue(is_unique("qwertyuiopasdfghjklzxcvbnm"))
-		self.assertTrue(is_unique("`1234567890-=][poiuytrewasdfghjkl;'/.,mnbvcxz"))
-		self.assertTrue(is_unique("qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM"))
-		self.assertTrue(is_unique("~!@#$%^&*()_+QWERTYUIOPSDFGHJKLZXCVBNM"))
-		self.assertTrue(is_unique(""))
-		self.assertTrue(is_unique("OH oh"))
-	
-	def test_false_words(self):
-		""" Words that should not pass """
+    def test_passing_words(self):
+        """ Normal passing words"""
+        self.assertTrue(is_unique(" "))
+        self.assertTrue(is_unique("abc"))
+        self.assertTrue(is_unique("yole"))
+        self.assertTrue(is_unique("aeiou"))
+        self.assertTrue(is_unique("qwertyuiopasdfghjklzxcvbnm"))
+        self.assertTrue(is_unique("`1234567890-=][poiuytrewasdfghjkl;'/.,mnbvcxz"))
+        self.assertTrue(is_unique("qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM"))
+        self.assertTrue(is_unique("~!@#$%^&*()_+QWERTYUIOPSDFGHJKLZXCVBNM"))
+        self.assertTrue(is_unique(""))
+        self.assertTrue(is_unique("OH oh"))
 
-		self.assertFalse(is_unique("AA"))
-		self.assertFalse(is_unique("aa"))
-		self.assertFalse(is_unique("!!"))
-		self.assertFalse(is_unique("A,A"))
-		self.assertFalse(is_unique("!@#$%^&*()(*&^%TGHJU^Y%T$REWFDSFGTY&U%^EY%TSERAFSDZVFGSTARWEFSDFRAETARGDZFSE"))
-		self.assertFalse(is_unique("ABBA"))
-		self.assertFalse(is_unique("!#$%@#^@#%@#$%"))
-		self.assertFalse(is_unique("Complication is Futie"))
-		self.assertFalse(is_unique("I love a dove"))
-		self.assertFalse(is_unique("  "))
-		self.assertFalse(is_unique("AlA"))
+    def test_false_words(self):
+        """ Words that should not pass """
+
+        self.assertFalse(is_unique("AA"))
+        self.assertFalse(is_unique("aa"))
+        self.assertFalse(is_unique("!!"))
+        self.assertFalse(is_unique("A,A"))
+        self.assertFalse(is_unique("!@#$%^&*()(*&^%TGHJU^Y%T$REWFDSFGTY&U%^EY%TSERAFSDZVFGSTARWEFSDFRAETARGDZFSE"))
+        self.assertFalse(is_unique("ABBA"))
+        self.assertFalse(is_unique("!#$%@#^@#%@#$%"))
+        self.assertFalse(is_unique("Complication is Futie"))
+        self.assertFalse(is_unique("I love a dove"))
+        self.assertFalse(is_unique("  "))
+        self.assertFalse(is_unique("AlA"))
+
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
